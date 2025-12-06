f061778014aaa11be139feeed3fb608c0be19409
umisan <t.umisan6@gmail.com>
2025-12-06 20:35:52 +0900

    initialize spring boot

---
+++ src/main/java/com/uminotech/hira/HiraApplication.java
@@ -0,0 +1,13 @@
+package com.uminotech.hira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uminotech/hira/HiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uminotech.hira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
